537505d62e304436059e8645a8b198f6f2a3d33a
Joshua Pham <joshua.pham@gmail.com>
2015-02-22 06:54:55 -0800

    test of vector; expression to type of scale.

--- js/dynamic_arp.js
+++ js/dynamic_arp.js
@@ -9,11 +9,40 @@ var minSev = [0, 3, 7, 10, 12, 15, 19, 22];
 var octaves = [-24, -12, 0, 12, 24, 36, 48];
 var direction = 0;*/
 
-/*
-var bass = new Tone.MonoSynth();
-bass.setPreset("Bassy");
-bass.setVolume(-10);
-bass.toMaster();*/
+var test = [3, 1, 2, 5, 7, 4, 3];
+test = test.toVector();
+test = test.sma(3);
+console.log(test);
+console.log(test.delta().distribution('relative'));
+
+var lv3_scale = [0, 4, 7, 12, 16, 19, 24, 28, 31]; // major
+var lv2_scale = [0, 2, 4, 7, 9, 12, 14, 16, 19, 21, 24, 26, 28, 31, 33, 36]; // penta
+var lv1_scale = [0, 3, 7, 12, 15, 19, 24, 27, 31]; //minor
+var semitones = ["C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2", "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4"];
+
+function genSequence(lv) {
+  var sequence = new Array();
+  var scaleName = "lv" + lv + "_scale";
+  var index = 0;
+  for (var i = 0; i < 8; i++) {
+    //index = Math.floor(Math.random()*5);
+    //console.log(eval(scaleName)[index]);
+    sequence.push(
+      [ (i + "*8n").toString(),
+        [ semitones[ eval(scaleName)[index++] ] ]
+      ]
+    );
+  }
+  console.log(scaleName);
+  console.log(sequence);
+  return sequence;
+}
+
+var seq;
+
+var reverb = new Tone.Freeverb();
+reverb.toMaster()
+reverb.setWet(0.2);
 
 var kit = new Tone.MultiSampler({
   //"drums": "../sounds/dancebeat.mp3"
@@ -21,41 +50,24 @@ var kit = new Tone.MultiSampler({
   "snare" : "../sounds/snare.mp3",
   "hh" : "../sounds/hh.mp3",
 }, function(){
-  //after the sounds are loaded, add the transport controls
-/*  $("#Loading").remove();
-  startButton.enable();*/
   Tone.Transport.start();
 });
 kit.setVolume(-8);
 kit.toMaster();
 
-var Score = {
-  "kick" : ["0", "0:2:2", "0:3:1"],
-  //use any Tone.Time representation or expression
-  "snare" : ["4n*1", "4n*3"],
-  "hh" : ["0*8n", "1*8n", "2*8n", "3*8n", "4*8n", "5*8n", "6*8n", "7*8n"],
-  //"drums": ["0"],
-  //if the array is composed of other arrays time is the first value
-  //the rest of the values are given to the callback in order
-  //"bass" : [["0:0", "C2", "2n"], ["0:3:2", "C3", "8n"]],
-  "keys" : [["0:0:0", ["C3"]], ["0:0:3", ["C3"]], ["0:1:2", ["C3"]],
-  ["0:2:0", ["C3"]], ["0:2:3", ["C3"]], ["0:3:2", ["C3"]]],
-  "midIntensity" : [["0:0:1", ["E3"]], ["0:1:0", ["E3"]], ["0:1:3", ["E3"]],
-  ["0:2:1", ["E3"]], ["0:3:0", ["E3"]], ["0:3:3", ["E3"]]],
-  "hiIntensity" : [["0:0:2", ["G3"]], ["0:1:1", ["G3"]], ["0:2:0", ["G3"]],
-  ["0:2:2", ["G3"]], ["0:3:1", ["G3"]], ["1:0:0", ["G3"]]]
-};
 
+var arp = new Tone.PolySynth(1, Tone.FMSynth)
+arp.toMaster();
+arp.connect(reverb);
 
-var keys = new Tone.PolySynth(1, Tone.FMSynth)
-keys.toMaster();
-var midIntensity = new Tone.PolySynth(1, Tone.FMSynth)
-midIntensity.toMaster();
-var hiIntensity = new Tone.PolySynth(1, Tone.FMSynth)
-hiIntensity.toMaster();
+/*var lv1 = new Tone.PolySynth(1, Tone.FMSynth)
+lv1.toMaster();
+var lv2 = new Tone.PolySynth(1, Tone.FMSynth)
+lv2.toMaster();
+var lv3 = new Tone.PolySynth(1, Tone.FMSynth)
+lv3.toMaster();*/
 
 //create events for all of the notes
-Tone.Note.parseScore(Score);
 
 //route the note channels
 Tone.Note.route("bass", function(time, note, duration){
@@ -64,29 +76,37 @@ Tone.Note.route("bass", function(time, note, duration){
 
 var DEF_VOLUME = -40;
 
-Tone.Note.route("keys", function(time, value){
+Tone.Note.route("arp", function(time, value){
   var velocity = Math.random() * 0.5 + 0.4;
   for (var i = 0; i < value.length; i++) {
-    keys.triggerAttackRelease(value[i], "16n", time, velocity);
-    //keys.setVolume(DEF_VOLUME);
+    arp.triggerAttackRelease(value[i], "32n", time, velocity);
+    arp.setVolume(-5);
   }
 });
 
-Tone.Note.route("midIntensity", function(time, value){
+/*Tone.Note.route("lv1", function(time, value){
   var velocity = Math.random() * 0.5 + 0.4;
   for (var i = 0; i < value.length; i++) {
-    midIntensity.triggerAttackRelease(value[i], "16n", time, velocity);
-    //midIntensity.setVolume(DEF_VOLUME);
+    lv1.triggerAttackRelease(value[i], "16n", time, velocity);
+    //lv1.setVolume(DEF_VOLUME);
   }
 });
 
-Tone.Note.route("hiIntensity", function(time, value){
+Tone.Note.route("lv2", function(time, value){
   var velocity = Math.random() * 0.5 + 0.4;
   for (var i = 0; i < value.length; i++) {
-    hiIntensity.triggerAttackRelease(value[i], "16n", time, velocity);
-    //hiIntensity.setVolume(DEF_VOLUME);
+    lv2.triggerAttackRelease(value[i], "16n", time, velocity);
+    //lv2.setVolume(DEF_VOLUME);
   }
 });
+
+Tone.Note.route("lv3", function(time, value){
+  var velocity = Math.random() * 0.5 + 0.4;
+  for (var i = 0; i < value.length; i++) {
+    lv3.triggerAttackRelease(value[i], "16n", time, velocity);
+    //lv3.setVolume(DEF_VOLUME);
+  }
+});*/
 
 /*Tone.Note.route("drums", function(time){
   kit.triggerAttack("drums", time);
@@ -115,18 +135,30 @@ Parse.initialize("sItbGUy6bQbGS1dqE9J7hlCKI8YmYfRSz3hxkqH9", "5HVZFYNDsw2boymo2F
 
 var EmotivModulations = Parse.Object.extend("moodMetrics");
 var query =  new Parse.Query(EmotivModulations);
-query.descending("createdAt")
-query.limit(1)
+query.descending("createdAt");
+var smaLimit = 3;
+query.limit(smaLimit)
 function latestMod(modulateParm) {
   query.find({
     success: function(obj) {
-      var valence = obj[0].get("valence");
-      var excitement = obj[0].get("excitement");
+      var valence_ary = new Array(), excitement_ary = new Array();
+      obj.forEach(function(el) {
+        valence_ary.push(el.get("valence"));
+        excitement_ary.push(el.get("excitement"));
+      });
+      console.log(excitement_ary);
+      valence_ary = valence_ary.toVector();
+      excitement_ary = excitement_ary.toVector();
+      valence_ary = valence_ary.sma(smaLimit);
+      excitement_ary = excitement_ary.sma(smaLimit);
+      console.log(excitement_ary);
+      console.log(excitement_ary.delta());
+      var valence = valence_ary[0];
+      var excitement = excitement_ary[0];
       modulateParm({"valence": valence, "excitement": excitement});
     }
   });
 }
-//var modulationQuery = new Parse.Query(EmotivModulations);
 
 var MIN_HEARING = -15;
 var MAX_HEARING = -2;
@@ -135,9 +167,9 @@ var MAX_HEARING = -2;
 /*var low_minTH = 0;
 var mid_minTH = 4;
 var hi_minTH = 8;*/
-var low_minTH = 0.2;
-var mid_minTH = 0.3;
-var hi_minTH = 0.6;
+var low_minTH = 0.4;
+var mid_minTH = 0.55;
+var hi_minTH = 0.7;
 
 
 // Poll for Emotiv changes
@@ -146,44 +178,55 @@ Tone.Transport.setInterval(function() {
     console.log("excitement:" + val.excitement + " valence: " + val.valence);
 
     // Volume modulated by valence
-    if (val.valence > low_minTH) {
+    if (val.valence > low_minTH && val.valence < mid_minTH) {
       console.log("valence > LOW")
-      keys.setVolume(-12);
+      //lv1.setVolume(-12);
+      seq = genSequence(1);
     }
-    if (val.valence > mid_minTH) {
+    else if (val.valence > mid_minTH && val.valence < hi_minTH) {
       console.log("valence > MID")
-      midIntensity.setVolume(-12)
-    };
-    if (val.valence > hi_minTH) {
-      console.log("valence > HI")
-      hiIntensity.setVolume(-12);
+      //lv2.setVolume(-12)
+      seq = genSequence(2);
     }
+    else if (val.valence > hi_minTH) {
+      console.log("valence > HI")
+      //lv3.setVolume(-12);
+      seq = genSequence(3);
+    }
+
+    var Score = {
+      "kick" : ["0", "0:2:2", "0:3:1"],
+      //use any Tone.Time representation or expression
+      "snare" : ["4n*1", "4n*3"],
+      "hh" : ["0*8n", "1*8n", "2*8n", "3*8n", "4*8n", "5*8n", "6*8n", "7*8n"],
+      //"drums": ["0"],
+      //if the array is composed of other arrays time is the first value
+      //the rest of the values are given to the callback in order
+      
+      "arp": seq
+
+      // below are hardcoded sequences (original arp)
+    /*  "lv1" : [["0:0:0", ["C3"]], ["0:0:3", ["C3"]], ["0:1:2", ["C3"]],
+      ["0:2:0", ["C3"]], ["0:2:3", ["C3"]], ["0:3:2", ["C3"]]],
+      "lv2" : [["0:0:1", ["E3"]], ["0:1:0", ["E3"]], ["0:1:3", ["E3"]],
+      ["0:2:1", ["E3"]], ["0:3:0", ["E3"]], ["0:3:3", ["E3"]]],
+      "lv3" : [["0:0:2", ["G3"]], ["0:1:1", ["G3"]], ["0:2:0", ["G3"]],
+      ["0:2:2", ["G3"]], ["0:3:1", ["G3"]], ["1:0:0", ["G3"]]]*/
+    };
+
+    Tone.Note.parseScore(Score);
 
 
     // Cutoff modulated by excitement
     // TODO: use cutoff instead of volume
     var excitement = -20 + ((val.excitement - 0.5) * 35);
-    //kit.setVolume(excitement);
+    kit.setVolume(-40);
     Tone.Transport.setBpm(10 + val.valence * 120);
     console.log("adjusted excitement: " + excitement );
     console.log("\n");
   })
 }, "1:0");
 
-/*// GUI //
-
-var content = $("#Content");
-new GUI.TopBar(Tone);
-
-//the transport controls
-var startButton = new GUI.Checkbox(content, function(down){
-  if (down){
-    Tone.Transport.start();
-  } else {
-    Tone.Transport.stop();
-  }
-}, "start", "stop");
-startButton.disable();*/
 
 var stopped = false;
 $('#startAudio').click(function() {
